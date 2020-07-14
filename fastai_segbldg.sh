b3b6ed9630eb05f5dd34579493b567fd51c5c45f
hanfu <8006346+hanfu@users.noreply.github.com>
2020-07-14 22:36:37 +0800

    Update fastai_segbldg.sh

--- fastai_segbldg.sh
+++ fastai_segbldg.sh
@@ -2,4 +2,4 @@ mkdir /content/fastai
 mkdir /content/fastai/segbldg
 PROJDIR=/content/fastai/segbldg
 git clone https://github.com/GeospatialGeeks/Satellite-Image-Building-Segmentation $PROJDIR/nbs
-unzip /drive/My\ Drive/Fused\ Dataset.zip $PROJDIR/data
+unzip /content/drive/My\ Drive/Fused\ Dataset.zip $PROJDIR/data
