80f35846eaaffd33bc9cd4b5778f631235fe0bc8
hanfu <8006346+hanfu@users.noreply.github.com>
2021-01-03 11:31:04 +0000

    add dlwpt tutorial

--- colab.sh
+++ colab.sh
@@ -17,5 +17,6 @@ echo "export INITENV=/content/initenv" >> ~/.bashrc
 echo "export gitc='git -C /content/initenv'" >> ~/.bashrc
 echo "export bucket='s3://colab-freetier210722'" >> ~/.bashrc
 echo "run jupyterlab at port 8888"
+echo "git clone https://github.com/deep-learning-with-pytorch/dlwpt-code.git /content/dlwpt"
 nohup jupyter lab --allow-root --no-browser --ip 0.0.0.0 --port 8888 &
 # use jupyter notebook list, jobs, bg, fg to control
